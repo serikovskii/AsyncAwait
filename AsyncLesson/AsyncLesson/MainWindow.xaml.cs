ddb8805399f5dfe5509a436b3946b1d755b3102b
Azat <46015824+serikovskii@users.noreply.github.com>
2019-06-10 11:55:53 +0600

    theory

---
+++ AsyncLesson/AsyncLesson/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AsyncLesson
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void HandleNameButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (await SendViaEmail(nameTextBox.Text, "tokaev@inbox.ru", "город мой Астана"))
+            {
+                await SendDatabase(nameTextBox.Text);
+                MessageBox.Show("служу СССР");
+            }
+        }
+
+        private Task SendDatabase(string name)
+        {
+            return Task.Run(() =>
+            {
+                //STUB
+                Thread.Sleep(5000);
+            });
+        }
+
+        private Task<bool> SendViaEmail(string name, string to, string title)
+        {
+            return Task.Run(() =>
+            {
+                //STUB
+                Thread.Sleep(3000);
+                return true;
+            });
+        }
+    }
+}
